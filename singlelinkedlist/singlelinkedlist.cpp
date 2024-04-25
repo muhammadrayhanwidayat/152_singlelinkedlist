b254e1f5bf29121bee6cf045a0b144d5866c31a7
muhammadrayhanwidayat <murayat99@gmail.com>
2024-04-25 10:04:49 +0700

    membuat struct node dan membuat node dimulai dari null

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
 
+
+
+struct Node {
+    int nomhs;
+    string nmae;
+    Node* next;
+};
+
+Node* START = NULL;
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main()
 {
     
