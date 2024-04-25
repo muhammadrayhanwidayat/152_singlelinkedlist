c374d78a6ecc43be5de4ba2ed6c96adce9bc2053
muhammadrayhanwidayat <murayat99@gmail.com>
2024-04-25 11:05:14 +0700

    membuat pengondisian else

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -107,7 +107,21 @@ void searchData() {
         system("pause");
         system("cls");
         return;
+    } 
+    else {
+        int nim;
+        cout << "Masukkan NIM: ";
+        cin >> nim;
+        Node* currentNode = START;
+
+        while (currentNode != NULL) {
+            if (currentNode->noMhs == nim) {
+                cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+                return;
+            }
+        }
     }
+
 
     
     
