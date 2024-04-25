0bdacb3f2c6bd5cd8e2e67dd5f98f75baeaa7857
muhammadrayhanwidayat <murayat99@gmail.com>
2024-04-25 11:02:53 +0700

    membuat prosedur search data

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -100,6 +100,15 @@ void traverse() {
         }
     }
 }
+
+void searchData() {
+    if (listEmpty()) {
+        cout << "List Kosong" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
+
     
     
 
