dc2d9bd87cb133475d240bd66c30525829035b40
muhammadrayhanwidayat <murayat99@gmail.com>
2024-04-25 11:06:18 +0700

    menambahkan output

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -119,7 +119,9 @@ void searchData() {
                 cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
                 return;
             }
+            currentNode = currentNode->next;
         }
+        cout << "Data tidak ditemukan" << endl;
     }
 
 
