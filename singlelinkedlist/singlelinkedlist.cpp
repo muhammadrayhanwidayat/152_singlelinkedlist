54855e5e2a84a1d8d7bb69143231bba4f2b171a0
muhammadrayhanwidayat <murayat99@gmail.com>
2024-04-25 10:58:28 +0700

    membuat fungsi list empty

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -79,6 +79,12 @@ bool deleteNode(int nim) {
         START = current->next;
     return true;
 }
+bool listEmpty() {
+    if (START == NULL) 
+        return true;
+    else 
+        return false;
+ }
     
 
 
