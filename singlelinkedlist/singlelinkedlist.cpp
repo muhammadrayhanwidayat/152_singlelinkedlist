70225d1eef87751c334bffb4b638356a9f2b8094
muhammadrayhanwidayat <murayat99@gmail.com>
2024-04-25 10:08:16 +0700

    memasukan variabel dalam prosedur addNode

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -5,13 +5,24 @@ using namespace std;
 
 struct Node {
     int nomhs;
-    string nmae;
+    string name;
     Node* next;
 };
 
 Node* START = NULL;
 
+void addNode() {
+    int nim;
+    string nama;
+    Node* nodeBaru = new Node();
+    cout << "masukan NIM: ";
+    cin >> nim;
+    cout << "masukan Nama: ";
+    cin >> nama;
+    nodeBaru->nomhs = nim;
+    nodeBaru->name = nama;
 
+}
 
 
 
