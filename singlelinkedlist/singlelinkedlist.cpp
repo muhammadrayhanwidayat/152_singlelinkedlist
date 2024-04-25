b31ae4404562cc55884615d187341c71d250fb20
muhammadrayhanwidayat <murayat99@gmail.com>
2024-04-25 10:48:30 +0700

    memperbaiki kesalahan logika

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -50,11 +50,14 @@ while ((current != NULL) && (nim >= current->noMhs)) {
     previous->next = nodeBaru;
 }
 
-bool searchNode(int nim, Node current, Node* previous) {
+bool searchNode(int nim, Node* current, Node* previous) {
+    previous = START;
+    current = START;
     while (current != NULL && nim > current->noMhs) {
         previous = current;
         current = current->next;
     }
+
     if (current == NULL) {
         return false;
     }
